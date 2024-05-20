3f4dbd70bb9b4e93a35dbc483e3fecaa67e3d2a7
Mariana Freitas <m.arisftsabreu@gmail.com>
2024-05-20 13:36:00 -0300

    feat: adicionando validações de número e de caracteres espeiciais

--- cypress/e2e/campo-nome.cy.js
+++ cypress/e2e/campo-nome.cy.js
@@ -7,12 +7,32 @@ describe('Login - Caminho feliz', () => {
 
     it('Campo de nome com caracteres adequados', () => {
         const nome = faker.name.findName()
-        
+
         cy.get(':nth-child(3) > :nth-child(2) > .field > input').type(nome)
 
         cy.get('.button-register').click()
 
         cy.get(':nth-child(3) > :nth-child(2) > .field > .alert-error').should('not.exist')
-        
+
+    });
+
+    it('Campo de nome com números', () => {
+        cy.get(':nth-child(3) > :nth-child(2) > .field > input').type('123456789')
+
+        cy.get('.button-register').click()
+
+        cy.get(':nth-child(3) > :nth-child(2) > .field > .alert-error')
+            .should('be.visible')
+            .should('have.value', 'Caracteres numéricos não são aceitos neste campo')
+    });
+
+    it('Campo de nome com caracteres especiais', () => {
+        cy.get(':nth-child(3) > :nth-child(2) > .field > input').type('!@#$%*()')
+
+        cy.get('.button-register').click()
+
+        cy.get(':nth-child(3) > :nth-child(2) > .field > .alert-error')
+            .should('be.visible')
+            .should('have.value', 'Caracteres especiais não são aceitos neste campo')
     });
 });
